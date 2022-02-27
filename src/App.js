980afe23274ba1093fba860b6d4f1ba8e5a27e69
Kai Shi <shi.kai@outlook.com>
2022-02-27 12:46:45 -0500

    create random cat facts app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import { Card, Button } from "react-bootstrap";
 
-function App() {
+export default function App() {
+  const [catImg, setCatImg] = useState("");
+  const [catFact, setCatFact] = useState({});
+
+  useEffect(() => {
+    requestCatInfo();
+  }, []);
+
+  const requestCatInfo = () => {
+    fetch("https://cataas.com/cat?json=true")
+      .then((res) => res.json())
+      .then((json) => setCatImg(json))
+      .catch((error) => console.warn(error));
+
+    fetch("https://cat-fact.herokuapp.com/facts/random?animal_type=cat")
+      .then((res) => res.json())
+      .then((json) => setCatFact(json))
+      .catch((error) => console.warn(error));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="d-flex justify-content-center">
+      <Card className="m-3" style={{ width: "24rem" }}>
+        <Card.Img variant="top" src={`https://cataas.com/${catImg.url}`} />
+        <Card.Body>
+          <Card.Text>{catFact.text}</Card.Text>
+          <Button variant="primary" onClick={requestCatInfo}>
+            🐈 Get a cat fact
+          </Button>
+        </Card.Body>
+      </Card>
     </div>
   );
 }
-
-export default App;
